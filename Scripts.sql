1fbe1bdd443a975dbdf9a11996d0f03a41a8c5b5
Jesse <80411319+JayKay-15@users.noreply.github.com>
2021-04-04 15:56:55 -0500

    Update Scripts.sql

--- Scripts.sql
+++ Scripts.sql
@@ -1,5 +1,5 @@
 SELECT
-	c.customer_id,
+  c.customer_id,
   c.first_name,
   c.last_name,
   sum(oi.quantity * oi.unit_price) AS total_sale
@@ -8,7 +8,7 @@ JOIN orders o USING (customer_id)
 JOIN order_items oi USING (order_id)
 WHERE state = 'VA'
 GROUP BY
-	c.customer_id,
+  c.customer_id,
   c.first_name,
   c.last_name
 HAVING total_sales > 100
