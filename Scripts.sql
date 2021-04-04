1e18aaac89835dfa964c38fa3c386596c93b09ca
Jesse <80411319+JayKay-15@users.noreply.github.com>
2021-04-04 16:10:19 -0500

    Update Scripts.sql

--- Scripts.sql
+++ Scripts.sql
@@ -30,7 +30,7 @@ ORDER BY first_name
 UPDATE orders
 SET comments = 'Gold Customer'
 WHERE customer_id IN 
-				        (SELECT customer_id
+	       (SELECT customer_id
                 FROM customers
                 WHERE points > 3000)
 
