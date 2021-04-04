2f74e6de8220e97c1254bd325871ab7556adf686
Jesse <80411319+JayKay-15@users.noreply.github.com>
2021-04-04 16:09:48 -0500

    Update Scripts.sql

--- Scripts.sql
+++ Scripts.sql
@@ -1,3 +1,41 @@
+-- Return customer type (bronze, silver, gold) based on loyalty points
+
+SELECT 
+  customer_id,
+  first_name,
+  points,
+  'Bronze' AS type
+FROM customers
+WHERE points < 2000
+UNION
+SELECT 
+  customer_id,
+  first_name,
+  points,
+  'Silver' AS type
+FROM customers
+WHERE points BETWEEN 2000 AND 3000
+UNION
+SELECT 
+  customer_id,
+  first_name,
+  points,
+  'Gold' AS type
+FROM customers
+WHERE points > 3000
+ORDER BY first_name
+
+-- Add comments to customers with over 3000 loyalty points
+
+UPDATE orders
+SET comments = 'Gold Customer'
+WHERE customer_id IN 
+				        (SELECT customer_id
+                FROM customers
+                WHERE points > 3000)
+
+-- Customers in Virginia who spent more than $100
+
 SELECT
   c.customer_id,
   c.first_name,
@@ -12,3 +50,9 @@ GROUP BY
   c.first_name,
   c.last_name
 HAVING total_sales > 100
+
+--
+
+
+                
+
