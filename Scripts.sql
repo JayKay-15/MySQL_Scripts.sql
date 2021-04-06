a3cd9ab050a50b34618308323fbcc877dfc4e518
Jesse <80411319+JayKay-15@users.noreply.github.com>
2021-04-06 12:02:03 -0500

    Update Scripts.sql

--- Scripts.sql
+++ Scripts.sql
@@ -97,4 +97,28 @@ SELECT
     (SELECT total_sales) - (SELECT average) AS difference
 FROM clients c
                 
---
+-- Subquery in FROM clause; simple query
+
+SELECT *
+FROM (
+    SELECT 
+	client_id,
+	name,
+	(SELECT SUM(invoice_total) 
+		FROM invoices
+		WHERE client_id = c.client_id) AS total_sales,
+	(SELECT AVG(invoice_total)
+		FROM invoices) AS average,
+	(SELECT total_sales) - (SELECT average) AS difference
+     FROM clients c
+) AS sales_summary
+WHERE total_sales IS NOT NULL
+
+-- 
+
+
+
+
+
+
+
