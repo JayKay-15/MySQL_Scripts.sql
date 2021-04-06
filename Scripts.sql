7c119a6923b927575704ef3c92852ed37797b83d
Jesse <80411319+JayKay-15@users.noreply.github.com>
2021-04-05 20:18:13 -0500

    Update Scripts.sql

--- Scripts.sql
+++ Scripts.sql
@@ -52,7 +52,19 @@ GROUP BY
   c.last_name
 HAVING total_sales > 100
 
---
+-- Select all customer who have purchases product_id 3
+
+SELECT 
+  customer_id, 
+  first_name, 
+  last_name
+FROM customers
+WHERE customer_id IN (
+	SELECT o.customer_id
+	FROM order_items oi
+    	JOIN orders o USING (order_id)
+    	WHERE product_id = 3
+)
 
 
                 
