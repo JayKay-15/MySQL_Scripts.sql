c8b58f75ab91a4ee963b6fa62eca1626058f32fb
Jesse <80411319+JayKay-15@users.noreply.github.com>
2021-04-08 13:30:26 -0500

    Update Scripts.sql

--- Scripts.sql
+++ Scripts.sql
@@ -140,9 +140,27 @@ BEGIN
     AND p.payment_method = IFNULL(payment_method_id, p.payment_method);
 END
 
+-- Stored procedure with validation
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `make_payment`(
+    invoice_id INT,
+    payment_amount DECIMAL (9,2),
+    payment_date DATE
+)
+BEGIN
+	IF payment_amount <= 0 THEN
+	SIGNAL SQLSTATE '22003' 
+        SET MESSAGE_TEXT = 'Invalid payment amount';
+	END IF;
+    
+UPDATE invoices i
+    SET 
+	i.payment_total = payment_amount,
+        i.payment_date = payment_date
+	WHERE i.client_id = client_id;
+END
+
 -- 
-
-
 
 
 
