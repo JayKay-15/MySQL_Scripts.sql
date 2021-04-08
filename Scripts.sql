6b79b7a6db99f46cf99fd79cbaa38b9981d60efa
Jesse <80411319+JayKay-15@users.noreply.github.com>
2021-04-08 13:17:33 -0500

    Update Scripts.sql

--- Scripts.sql
+++ Scripts.sql
@@ -127,7 +127,20 @@ SELECT
 FROM customers
 ORDER BY points DESC
 
---
+-- Create a stored procedure
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `get_payments`(
+	client_id INT, 
+	payment_method_id TINYINT
+)
+BEGIN
+    SELECT *
+    FROM payments p
+    WHERE p.client_id = IFNULL(client_id, p.client_id) 
+    AND p.payment_method = IFNULL(payment_method_id, p.payment_method);
+END
+
+-- 
 
 
 
